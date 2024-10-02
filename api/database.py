e3405407e5543c1d5f43a92c29dc76f28014bf02
unknown <jonathanmbaya13@gmail.com>
2024-10-02 23:51:53 +0200

    MAJ

--- api/database.py
+++ api/database.py
@@ -1,17 +1,7 @@
-import urllib.parse
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
-# Mot de passe avec des caractères spéciaux doit être encodé
-username = "krachenbeatz"
-password = "Sonia220364@@@"
-encoded_password = urllib.parse.quote_plus(password)
-
-# Construisez l'URI avec le mot de passe encodé
-mongo_uri = f"mongodb+srv://{username}:{encoded_password}@epusa.ebpdb.mongodb.net/"
-
-# Initialisez le client MongoDB
-client = MongoClient(mongo_uri)
-
-# Accédez à votre base de données et à votre collection
-database = client["epusa"]
+mongo_uri = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(mongo_uri)
+database = client["e_p_usa"]
 collection = database["names"]
